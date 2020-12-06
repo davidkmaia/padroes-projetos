730533e4fc3c294041d94614c8fcd6a561191c07
David Maia <davidkmaia@gmail.com>
2020-12-06 15:14:43 -0300

    chain of reponsability: se aumentar os tipos de desconto, aumenta o metodo da classe calculador de desconto

---
+++ src/behabioral/chainOfResponsability/ex1/Item.java
@@ -0,0 +1,28 @@
+package behabioral.chainOfResponsability.ex1;
+
+public class Item {
+	
+	private String nome;
+	private double valor;
+
+	public Item(String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+}
---
+++ src/behabioral/chainOfResponsability/ex1/Orcamento.java
@@ -0,0 +1,30 @@
+package behabioral.chainOfResponsability.ex1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Orcamento {
+
+	private double valor;
+	private List<Item> itens;
+
+	public Orcamento(double valor) {
+		this.valor = valor;
+		this.itens = new ArrayList<Item>();
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
+	}
+	
+	public void adicionaItem(Item item) {
+		itens.add(item);
+	}
+
+	
+}
